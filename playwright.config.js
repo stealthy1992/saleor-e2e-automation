6674f42498ad3bfc65d3346bcca0b2922dd21ef3
stealthy1992 <rehman.1992@hotmail.com>
2026-09-19 22:05:33 +0500

    changes to  results.json file path

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   testDir: './tests',
   workers: 2,
   retries: process.env.CI ? 2 : 0,
-  reporter: [['html', { open: 'never' }], ['json', { outputFile: 'results.json' }],  ['list']],
+  reporter: [['html', { open: 'never' }], ['json', { outputFile: 'test-results/results.json' }],  ['list']],
   use: {
     baseURL: process.env.SALEOR_API_URL || 'http://localhost:8000',
     trace: 'retain-on-failure',
